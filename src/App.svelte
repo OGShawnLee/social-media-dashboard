3a02235ce7d8103a8958379b16fcb23ea3ffefee
Shawn Lee <changoloco.evg@gmail.com>
2022-04-11 19:14:11 -0500

    :tada: feat: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<!-- Header -->
+Social Media Dashboard
+<!-- Followers -->
+Total Followers: 23,004
+<!-- Theme Button -->
+Dark Mode
+
+<!-- Facebook-->
+@nathanf
+<!-- Followers -->
+1987 Followers
+<!-- Today -->
+12 Today
+
+<!-- Twitter -->
+@nathanf
+<!-- Followers -->
+1044 Followers
+<!-- Today -->
+99 Today
+
+<!-- Instagram -->
+@realnathanf
+<!-- Followers -->
+11k Followers
+<!-- Today -->
+1099 Today
+
+<!-- Youtube -->
+Nathan F.
+<!-- Subscribers -->
+8239 Subscribers 144 Today
+
+<!-- Overview -->
+Overview - Today
+
+<!-- Facebook -->
+<!-- Views -->
+Page Views 87 3%
+
+<!-- Likes -->
+Likes 52 2%
+
+<!-- Instagram -->
+<!-- Likes -->
+Likes 5462 2257%
+<!-- Views -->
+Profile Views 52k 1375%
+
+<!-- Facebook -->
+<!-- Retweets -->
+Retweets 117 303%
+
+<!-- Likes -->
+Likes 507 553%
+
+<!-- Youtube -->
+<!-- LIkes -->
+Likes 107 19%
+<!-- Views -->
+Total Views 1407 12%
