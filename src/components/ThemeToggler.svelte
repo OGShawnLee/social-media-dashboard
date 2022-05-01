58b4a695389105bece4eafc5fa1fad46e7e7bf30
Shawn Lee <changoloco.evg@gmail.com>
2022-05-01 14:24:46 -0500

    :sparkles: feat: use malachite-ui switch component

--- src/components/ThemeToggler.svelte
+++ src/components/ThemeToggler.svelte
@@ -1,27 +1,28 @@
 <script>
+  import { Switch, SwitchDescription, SwitchLabel } from 'malachite-ui/components';
   import { useDarkTheme } from '../hooks';
 
   const theme = useDarkTheme('DARK', 'SOCIAL-DASHBOARD-OG-THEME-COLOR-KEY');
 
-  $: themeText = $theme === 'DARK' ? 'Light' : 'Dark';
-  $: thumbPosition = $theme === 'DARK' ? 'translate-x-1' : 'translate-x-9';
+  $: isDarkMode = $theme === 'DARK';
+  $: thumbPosition = isDarkMode ? 'translate-x-9' : 'translate-x-1';
 </script>
 
 <div class="py-6 | flex items-center justify-between gap-6">
-  <span class="text-sm font-bold light:text-blue-gray-750"> {themeText} Mode </span>
-  <button
-    class="relative h-8 w-16 | dark:(bg-gradient-to-r from-toggle-blue to-toggle-green) light:hover:(bg-gradient-to-r from-toggle-blue to-toggle-green) bg-toggle-light rounded-full"
-    aria-label="Toggle Color Theme"
-    aria-describedby="theme-toggler-description"
-    title="Toggle Color Theme"
-    on:click={theme.toggle}>
-    <span class="sr-only" id="theme-toggler-description">
-      Toggle Color Theme from dark mode to light mode and viceversa
-    </span>
-
-    <span
-      class="absolute h-6 w-6 min-w-6 | block | dark:bg-blue-gray-900 bg-white rounded-full | transform animation over-y-center {thumbPosition}" />
-  </button>
+  <Switch checked={isDarkMode} as="slot" let:button>
+    <SwitchLabel class="text-sm font-bold light:text-blue-gray-750" passive>Dark Mode</SwitchLabel>
+    <button
+      class="relative h-8 w-16 | dark:(bg-gradient-to-r from-toggle-blue to-toggle-green) light:hover:(bg-gradient-to-r from-toggle-blue to-toggle-green) bg-toggle-light rounded-full"
+      use:button
+      on:click={theme.toggle}>
+      <SwitchDescription class="sr-only">
+        Switch from Light to Dark Theme and vice versa
+      </SwitchDescription>
+      <span
+        aria-hidden="true"
+        class="absolute h-6 w-6 min-w-6 | block | dark:bg-blue-gray-900 bg-white rounded-full | transform animation over-y-center {thumbPosition}" />
+    </button>
+  </Switch>
 </div>
 
 <style>
